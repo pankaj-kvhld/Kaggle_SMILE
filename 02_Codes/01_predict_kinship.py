26a6bc27f68bd74907854fbe5222031d60fa5682
Pankaj Joshi <pankaj.joshi.in@gmail.com>
2019-05-19 07:22:50 +0200

    Negative examples, not finished

--- 02_Codes/01_predict_kinship.py
+++ 02_Codes/01_predict_kinship.py
@@ -1,20 +1,21 @@
-import dlib 
+import dlib
 from pathlib import Path
 import pandas as pd
 import os
 import numpy as np
 
-DATA_DIR = Path(__file__).parent.parent/"01_Data"
-FACE_REC_MODEL = DATA_DIR/"dlib_models"/"dlib_face_recognition_resnet_model_v1.dat"
-PREDICTOR = DATA_DIR/"dlib_models"/"shape_predictor_5_face_landmarks.dat"
+DATA_DIR = Path(__file__).parent.parent / "01_Data"
+FACE_REC_MODEL = DATA_DIR / "dlib_models" / "dlib_face_recognition_resnet_model_v1.dat"
+PREDICTOR = DATA_DIR / "dlib_models" / "shape_predictor_5_face_landmarks.dat"
 
 # Relationship data
-df_kinship = pd.read_csv(DATA_DIR/"train_relationships.csv")
+df_kinship = pd.read_csv(DATA_DIR / "train_relationships.csv")
 
 # Function to extract 128 vector for a given image
 detector = dlib.get_frontal_face_detector()
 sp = dlib.shape_predictor(str(PREDICTOR))
 facerec = dlib.face_recognition_model_v1(str(FACE_REC_MODEL))
+
 
 def img_2_128vec(img):
     """ Takes an image and retuns the 128 dimension vector
@@ -28,45 +29,57 @@ def img_2_128vec(img):
         print("More than one faces in the image")
         return -1
 
-
-# Construct all positive examples. 128 vecs of two related person are appened to make 
-# 1 vector of length 256. This is done exhaustively. So if person 1 is realated to person2 
-# and they have N and M images respectively, we will get N*M  positive examples. 
-all_positive_exs = np.empty((0, 2*128))
+# Construct negative examples
+# All the combination of non-kinship images from all family folders will be use.
+# If we get roughly as many positive samples, I will stick with this approach.
+all_negative_exs = np.empty((0, 2 * 128))
 
 for index, row in df_kinship.iterrows():
     
+
+
+# Construct all positive examples. 128 vecs of two related person are appened to make
+# 1 vector of length 256. This is done exhaustively. So if person 1 is realated to person2
+# and they have N and M images respectively, we will get N*M  positive examples.
+all_positive_exs = np.empty((0, 2 * 128))
+
+for index, row in df_kinship.iterrows():
+
     print(f"{index} out of {df_kinship.shape[0]}")
-    dir_p1 = DATA_DIR/"train"/row['p1']
-    dir_p2 = DATA_DIR/"train"/row['p2']
-    
+    dir_p1 = DATA_DIR / "train" / row["p1"]
+    dir_p2 = DATA_DIR / "train" / row["p2"]
+
     # Skip is the folders do not exit
     if not os.path.exists(dir_p1) or not os.path.exists(dir_p2):
         print(f"Data from {index} does not exist")
         continue
-        
+
     for f1 in os.listdir(dir_p1):
-        img_1 = dlib.load_rgb_image(str(dir_p1/f1))
+        img_1 = dlib.load_rgb_image(str(dir_p1 / f1))
         f1_128_vec = img_2_128vec(img_1)
-        
+
         if f1_128_vec == -1:
-            print(f'{dir_p1/f1} failed to process')
+            print(f"{dir_p1/f1} failed to process")
             continue
-        
+
         for f2 in os.listdir(dir_p2):
-            img_2 = dlib.load_rgb_image(str(dir_p2/f2))
+            img_2 = dlib.load_rgb_image(str(dir_p2 / f2))
             f2_128_vec = img_2_128vec(img_2)
-            
+
             if f2_128_vec == -1:
                 print(f"{dir_p2/f2} failes to process")
                 continue
-            
+
             # Flatten and join 128 vec of both images
             both_imgs = np.append(f1_128_vec, f2_128_vec)
             # Append to all_positive_exs
-            all_positive_exs = np.append(all_positive_exs, both_imgs.reshape((-1, 2*128)), axis=0)            
-   
-# 158 ,989 examples of kinshiop vectors
-np.save(DATA_DIR.parent/"03_Processed"/"all_positive_examples.npy", all_positive_exs )
+            all_positive_exs = np.append(
+                all_positive_exs, both_imgs.reshape((-1, 2 * 128)), axis=0
+            )
 
-# Construct negative examples
+# 158 ,989 examples of kinshiop vectors
+np.save(
+    DATA_DIR.parent / "03_Processed" / "all_positive_examples.npy", all_positive_exs
+)
+
+
